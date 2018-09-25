28de34fbcd42de8d657d52a2a7e9e41b0fe19296
Yutung Huang <milkytrace@hotmail.com>
2018-09-25 18:21:16 +0800

    Filter for reviews shorter than 100 characters

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -4,15 +4,18 @@ with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 10000 == 0:
+		if count % 50000 == 0:
 			print(len(data))
-print('檔案讀取完了，總共有',len(data),'筆資料')
+print('檔案讀取完了，總共有',len(data),'筆資料\n')
 
 sum_len = 0
 for d in data:
 	sum_len += len(d)
 avg = sum_len/len(data)
-print('算出來了，留言的平均長度是',avg,'字')
+print('算出來了，留言的平均長度是',avg,'字\n')
 
-
-
+filter = []
+for d in data:
+	if len(d) < 100:
+		filter.append(d)
+print('總共有',len(filter),'筆留言長度小於100\n')
