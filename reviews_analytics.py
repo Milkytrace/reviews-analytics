b107b10bdab554816d6f4147ac50c85ba1a85af2
Yutung Huang <milkytrace@hotmail.com>
2018-09-25 18:15:09 +0800

    Add file

---
+++ reviews_analytics.py
@@ -0,0 +1,18 @@
+data = []
+count = 0
+with open('reviews.txt','r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 10000 == 0:
+			print(len(data))
+print('檔案讀取完了，總共有',len(data),'筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+avg = sum_len/len(data)
+print('算出來了，留言的平均長度是',avg,'字')
+
+
+
