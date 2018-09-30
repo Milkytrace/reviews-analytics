8391aee4311fc2ca6655a502e46d0411b09eb9ad
Yutung Huang <milkytrace@hotmail.com>
2018-09-30 14:33:32 +0800

    add word count

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -28,5 +28,31 @@ print('總共有',len(good),'筆留言含有 excellent這個詞')
 
 nasty = [m for m in data if 'nasty' in m]
 print('其中有',len(nasty),'條留言有 nasty 這個詞')
-print(nasty[7])
+
+#文字計數
+wc = {} # word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] >100:
+		print(word, wc[word])
+
+while True:
+	 query = input('請問你想查什麼字:(退出請按 q)')
+	 if query == 'q':
+	 	print('感謝您使用本查詢功能')
+	 	break
+	 if query in wc:
+	 	print(query, ' 出現過的次數是', wc[query], '次')
+	 else:
+	 	print('這個字沒有出現過哦')
+
+
+
 
