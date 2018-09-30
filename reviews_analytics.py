aab7ca84f927a9b8df58f3fc4526ce45993d844e
Yutung Huang <milkytrace@hotmail.com>
2018-09-30 15:37:15 +0800

    Add progress bar

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,11 +1,14 @@
+import time
+import progressbar
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value=1000000)
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 50000 == 0:
-			print(len(data))
+		bar.update(count)
 print('檔案讀取完了，總共有',len(data),'筆資料\n')
 
 sum_len = 0
@@ -30,6 +33,7 @@ nasty = [m for m in data if 'nasty' in m]
 print('其中有',len(nasty),'條留言有 nasty 這個詞')
 
 #文字計數
+start_time = time.time()
 wc = {} # word_count
 for d in data:
 	words = d.split()
@@ -42,6 +46,8 @@ for d in data:
 for word in wc:
 	if wc[word] >100:
 		print(word, wc[word])
+end_time = time.time()
+print('花了', end_time - start_time, '秒')
 
 while True:
 	 query = input('請問你想查什麼字:(退出請按 q)')
